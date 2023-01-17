d486e48a3cb0c9f2009c02b243907c55e253627e
NoYoN1 <uilse1122@gmail.com>
2023-01-17 20:45:45 +0900

    main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.rout("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    # app.run(host="127.0.0.1", port=5550, debug=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)
